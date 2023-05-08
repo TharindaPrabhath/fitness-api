fbab5be65b0eb53c329bda925a5f010479b70f74
TharindaPrabhath <tharindahp@gmail.com>
2023-05-08 20:34:54 +0530

    first commit

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Filter for handling prisma client errors
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  //  app.useGlobalFilters(new PrismaExceptionFilter(httpAdapter));
+
+  await app.listen(5000);
+}
+bootstrap();
